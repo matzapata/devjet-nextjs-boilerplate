f7c718c16638ce1b3e758fcecd2c66461d1f9a87
Matias Zapata <matuzapata@gmail.com>
2022-09-23 15:55:19 -0300

    Initial usedevjet setup for nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import chakraTheme from "../utils/chakraTheme"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={chakraTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
